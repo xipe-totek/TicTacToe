b9a15b006a2e6284b9eb7a290ba4083199f0f5a1
sordes <ddfsjj@gmail.com>
2025-08-17 19:38:18 +0200

    Added 9 buttons

--- TicTacToe/TicTacToe/Models/MainWindowModel.cs
+++ TicTacToe/TicTacToe/Models/MainWindowModel.cs
@@ -3,6 +3,21 @@ namespace TicTacToe.Models;
 public class MainWindowModel
 {
     /// <summary>
+    /// Game field width
+    /// </summary>
+    public const int Width = 3;
+    
+    /// <summary>
+    /// Game field height
+    /// </summary>
+    public const int Height = 3;
+    
+    /// <summary>
+    /// Game field
+    /// </summary>
+    public string[,] GameField = new string[Height, Width];
+    
+    /// <summary>
     /// How many times user pressed the button
     /// </summary>
     public int CountValue;
--- TicTacToe/TicTacToe/ViewModels/MainWindowViewModel.cs
+++ TicTacToe/TicTacToe/ViewModels/MainWindowViewModel.cs
@@ -8,11 +8,11 @@ namespace TicTacToe.ViewModels;
 public partial class MainWindowViewModel : ViewModelBase
 {
     private MainWindowModel _mainWindowModel;
-    
+
     #region Binbale variables
-    
+
     #region CountValue
-    
+
     public int CountValue
     {
         get => _mainWindowModel.CountValue;
@@ -22,28 +22,41 @@ public partial class MainWindowViewModel : ViewModelBase
 
     #endregion
     
+    #region Game field
+
+    public string Button0
+    {
+        get => _mainWindowModel.GameField[0, 0];
+        
+        set => this.RaiseAndSetIfChanged(ref _mainWindowModel.GameField[0, 0], value);
+    }
+    
+    #endregion
+
     #region Count Direction
 
     private bool _isIncrement;
-    
+
     public bool IsIncrement
     {
         get => _isIncrement;
 
         set => this.RaiseAndSetIfChanged(ref _isIncrement, value);
     }
-    
+
     #endregion
-    
+
     #endregion
 
     #region Commands
-    
+
     public ReactiveCommand<Unit, Unit> OnCountButtonCommandPlus { get; }
     public ReactiveCommand<Unit, Unit> OnCountButtonCommandMinus { get; }
-    
+
     public ReactiveCommand<Unit, Unit> DoActionCommand { get; }
-    
+
+    public ReactiveCommand<int, Unit> GameButtonPressedCommand { get; }
+
     #endregion
 
     public MainWindowViewModel
@@ -63,21 +76,23 @@ public partial class MainWindowViewModel : ViewModelBase
         (x => x.CountValue,
             cv => cv > -10
         );
-        
+
         OnCountButtonCommandPlus = ReactiveCommand.Create(OnCountButtonPressedPlus, canExecuteCountCommandPlus);
         OnCountButtonCommandMinus = ReactiveCommand.Create(OnCountButtonPressedMinus, canExecuteCountCommandMinus);
         DoActionCommand = ReactiveCommand.Create(OnDoActionCommand);
+        
+        GameButtonPressedCommand = ReactiveCommand.Create<int>(OnGameButtonPressed);
     }
-    
+
     private void OnCountButtonPressedPlus()
     {
-        CountValue ++;
+        CountValue++;
         IsIncrement = true;
     }
-    
+
     private void OnCountButtonPressedMinus()
     {
-        CountValue --;
+        CountValue--;
         IsIncrement = false;
     }
 
@@ -91,6 +106,11 @@ public partial class MainWindowViewModel : ViewModelBase
         {
             CountValue--;
         }
+    }
+
+    private void OnGameButtonPressed(int index)
+    {
+        CountValue = index;
     }
 
 }
